334ce187a2ae898d1e95dc141f3595b459349fdf
QUANG_CTY <nnduyquang@gmail.com>
2017-10-26 10:47:32 +0700

    OK

--- local/app/Http/Controllers/HomePageController.php
+++ local/app/Http/Controllers/HomePageController.php
@@ -1,16 +1,1 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Employee;
-use App\Slider;
-use Illuminate\Http\Request;
-
-class HomePageController extends Controller
-{
-    public function getInfoHomepage(){
-        $emps = Employee::where('emp_is_work', '=', '1')->orderBy('emp_order', 'ASC')->get();
-        $sliders = Slider::where('slider_is_active', '=', '1')->orderBy('slider_order', 'DESC')->get();
-        return view('frontend.trangchu.index', compact('emps','sliders'));
-    }
-}
+<?phpnamespace App\Http\Controllers;use App\Employee;use App\Posts;use App\Slider;use Illuminate\Http\Request;class HomePageController extends Controller{    public function getInfoHomepage(){        $emps = Employee::where('emp_is_work', '=', '1')->orderBy('emp_order', 'ASC')->get();        $sliders = Slider::where('slider_is_active', '=', '1')->orderBy('slider_order', 'DESC')->get();        return view('frontend.trangchu.index', compact('emps','sliders'));    }    public function getContentGioiThieu(){        $content=Posts::where('path','=','gioi-thieu')->first();        return view('frontend.trangcon.gioithieu', compact('content'));    }}
