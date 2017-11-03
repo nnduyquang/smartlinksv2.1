947a32a0cd72d94de6668985191010a382fcf335
QUANG_CTY <nnduyquang@gmail.com>
2017-11-03 17:06:38 +0700

    ok

--- local/resources/views/frontend/trangcon/facebook.blade.php
+++ local/resources/views/frontend/trangcon/facebook.blade.php
@@ -24,8 +24,10 @@
             <div class="fads-title col-md-12">
                 <div class="row">
                     <div class="container">
-                        <div class="col-md-12 fads-around">
-                            <p class="fads-header">Tại Sao Chọn Chúng Tôi</p>
+                        <div class="row">
+                            <div class="col-md-12 fads-around">
+                                <p class="fads-header">Tại Sao Chọn Chúng Tôi</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -59,8 +61,10 @@
             <div class="fads-title col-md-12">
                 <div class="row">
                     <div class="container">
-                        <div class="col-md-12 fads-around">
-                            <p class="fads-header">Phản Hồi Khách Hàng</p>
+                        <div class="row">
+                            <div class="col-md-12 fads-around">
+                                <p class="fads-header">Phản Hồi Khách Hàng</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -71,7 +75,10 @@
                         <div class="col-md-6">
                             <div class="fads-feedback-box box-1 col-md-12">
                                 <div class="row">
-                                    <p class="fads-feedback-content">“Trước Khi Khách Hàng Sử Dụng Dịch Vụ Chúng Tôi, Họ Tự Chạy Quảng Cáo Và Tốn Khá Nhiều Tiền Cho Mỗi Page Like. Họ Quyết Định Liên Hệ Và Sử Dụng Dịch Vụ Của Smartlinks Trong 1 Tháng, Và Bây Giờ Chi Phí Cho Quảng Cáo Đã Giảm 50%”</p>
+                                    <p class="fads-feedback-content">“Trước Khi Khách Hàng Sử Dụng Dịch Vụ Chúng Tôi, Họ
+                                        Tự Chạy Quảng Cáo Và Tốn Khá Nhiều Tiền Cho Mỗi Page Like. Họ Quyết Định Liên Hệ
+                                        Và Sử Dụng Dịch Vụ Của Smartlinks Trong 1 Tháng, Và Bây Giờ Chi Phí Cho Quảng
+                                        Cáo Đã Giảm 50%”</p>
                                     <div class="fads-feedback-box-bottom col-md-12">
                                         <div class="col-md-4">
                                             {{ Html::image('images/temps/iconspeople_save-01.png','',array('class'=>'fads-fb-img-icon')) }}
@@ -86,7 +93,10 @@
                         <div class="col-md-6">
                             <div class="fads-feedback-box box-2 col-md-12">
                                 <div class="row">
-                                    <p class="fads-feedback-content">“Chúng Tôi Giúp Cho Một Website Thương Mại Điện Tử Tăng Trưởng Lượt Khách Hàng Tiếp Cận Lên Hàng Tháng Từ 30% Lên 80% Bằng Cách Sử Dụng Quảng Cáo Facebook. Giá Mỗi Lượt Khách Hàng Tiếp Cận Website Đã Giảm Xuống 70% So Với Ban Đầu”</p>
+                                    <p class="fads-feedback-content">“Chúng Tôi Giúp Cho Một Website Thương Mại Điện Tử
+                                        Tăng Trưởng Lượt Khách Hàng Tiếp Cận Lên Hàng Tháng Từ 30% Lên 80% Bằng Cách Sử
+                                        Dụng Quảng Cáo Facebook. Giá Mỗi Lượt Khách Hàng Tiếp Cận Website Đã Giảm Xuống
+                                        70% So Với Ban Đầu”</p>
                                     <div class="fads-feedback-box-bottom col-md-12">
                                         <div class="col-md-4">
                                             {{ Html::image('images/temps/iconspeople_save-02.png','',array('class'=>'fads-fb-img-icon')) }}
@@ -101,7 +111,9 @@
                         <div class="col-md-6">
                             <div class="fads-feedback-box box-3 col-md-12">
                                 <div class="row">
-                                    <p class="fads-feedback-content">“Một Trong Những Công Ty Chuyên Về Sản Phẩm Thời Trang Nam Đã Sử Dụng Dịch Vu Của Chúng Tôi Đã Đạt Thêm 1250 Khách Hàng Tiềm Năng Từ Quảng Cáo Facebook Và Quyết Định Ký Hợp Đồng 6 Tháng Với Chúng Tôi ”</p>
+                                    <p class="fads-feedback-content">“Một Trong Những Công Ty Chuyên Về Sản Phẩm Thời
+                                        Trang Nam Đã Sử Dụng Dịch Vu Của Chúng Tôi Đã Đạt Thêm 1250 Khách Hàng Tiềm Năng
+                                        Từ Quảng Cáo Facebook Và Quyết Định Ký Hợp Đồng 6 Tháng Với Chúng Tôi ”</p>
                                     <div class="fads-feedback-box-bottom col-md-12">
                                         <div class="col-md-4">
                                             {{ Html::image('images/temps/iconspeople_save-03.png','',array('class'=>'fads-fb-img-icon')) }}
@@ -116,7 +128,11 @@
                         <div class="col-md-6">
                             <div class="fads-feedback-box box-4 col-md-12">
                                 <div class="row">
-                                    <p class="fads-feedback-content">“Một Khách Hàng Đã Đầu Tư Hơn 10 Triệu Vào Quảng Cáo Facebook Trong 1 Tháng Nhưng Không Đạt Được Số Lượng Đơn Hàng Như Mong Muốn. Chúng Tôi Giúp Anh Ta Tăng Trưởng Doanh Số Bán Hàng Lên 700% Bằng Cách Sử Dụng Số Tiền Tương Tự Và Trang Facebook Của Khách Hàng Đã Có Hơn 80.000 Lượt Yêu Thích ”</p>
+                                    <p class="fads-feedback-content">“Một Khách Hàng Đã Đầu Tư Hơn 10 Triệu Vào Quảng
+                                        Cáo Facebook Trong 1 Tháng Nhưng Không Đạt Được Số Lượng Đơn Hàng Như Mong Muốn.
+                                        Chúng Tôi Giúp Anh Ta Tăng Trưởng Doanh Số Bán Hàng Lên 700% Bằng Cách Sử Dụng
+                                        Số Tiền Tương Tự Và Trang Facebook Của Khách Hàng Đã Có Hơn 80.000 Lượt Yêu
+                                        Thích ”</p>
                                     <div class="fads-feedback-box-bottom col-md-12">
                                         <div class="col-md-4">
                                             {{ Html::image('images/temps/iconspeople_save-04.png','',array('class'=>'fads-fb-img-icon')) }}
@@ -134,8 +150,10 @@
             <div class="fads-title col-md-12">
                 <div class="row">
                     <div class="container">
-                        <div class="col-md-12 fads-around">
-                            <p class="fads-header">Chúng Tôi Sẽ Giúp Bạn</p>
+                        <div class="row">
+                            <div class="col-md-12 fads-around">
+                                <p class="fads-header">Chúng Tôi Sẽ Giúp Bạn</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -143,17 +161,67 @@
             <div id="fads-serv" class="col-md-12">
                 <div class="row">
                     <div class="container">
-                        <div class="col-md-6">
-                           <ul class="fads-serv-left">
-                               <li>Setup Facebook campaigns</li>
-                               <li>Setup Facebook campaigns</li>
-                               <li>Setup Facebook campaigns</li>
-                           </ul>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="fads-serv-left">
+                                    <li>Cài Đặt Chiến Dịch Quảng Cáo Facebook</li>
+                                    <li>Nhắm Mục Tiêu Chính Xác Đối Tượng</li>
+                                    <li>Quản Lý Và Tối Ưu Hóa Giá Thầu</li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6">
+                                <ul class="fads-serv-right">
+                                    <li>Giám Sát Liên Tục Về Hiệu Suất</li>
+                                    <li>Xuất Báo Cáo Khi Kết Thúc Chiến Dịch</li>
+                                    <li>Thiết Kế Mẫu Content Bắt Mắt</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="col-md-6"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="fads-title col-md-12">
+                <div class="row">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 fads-around">
+                                <p class="fads-header">Xác Định Mục Tiêu Quảng Cáo</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="fads-objective" class="col-md-12">
+                <div class="row">
+                    <div class="container">
+                        <div class="col-md-4">
+                            {{ Html::image('images/temps/icons_save-05.png','',array('class'=>'fads-fb-img-icon')) }}
+                            <h4>Tăng Like Fanpage</h4>
+                        </div>
+                        <div class="col-md-4">
+                            {{ Html::image('images/temps/icons_save-06.png','',array('class'=>'fads-fb-img-icon')) }}
+                            <h4>Tăng Traffic Website</h4>
+                        </div>
+                        <div class="col-md-4">
+                            {{ Html::image('images/temps/icons_save-07.png','',array('class'=>'fads-fb-img-icon')) }}
+                            <h4>Tương Tác Bài Viết</h4>
+                        </div>
+                        <div class="col-md-4">
+                            {{ Html::image('images/temps/icons_save-08.png','',array('class'=>'fads-fb-img-icon')) }}
+                            <h4>Tăng Tỷ Lệ Ra Đơn Hàng</h4>
+                        </div>
+                        <div class="col-md-4">
+                            {{ Html::image('images/temps/icons_save-09.png','',array('class'=>'fads-fb-img-icon')) }}
+                            <h4>Tìm Kiếm Khách Hàng Tìm Năng</h4>
+                        </div>
+                        <div class="col-md-4">
+                            {{ Html::image('images/temps/icons_save-10.png','',array('class'=>'fads-fb-img-icon')) }}
+                            <h4>Tăng Tỷ Lệ Tải Ứng Dụng</h4>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    @include('frontend.common.LienHeBaoGia')
 @stop
