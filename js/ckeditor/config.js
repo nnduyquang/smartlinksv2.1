116a8f2724aef9d4792364b4df18429b500d6012
QUANG_CTY <nnduyquang@gmail.com>
2018-01-15 17:51:47 +0700

    ok

--- js/ckeditor/config.js
+++ js/ckeditor/config.js
@@ -4,6 +4,7 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
+    config.entities_latin = false;
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
